9125242e724266a96dd7a252af8d78ead9344a98
Shadow-Storm <114849390+ShadowStorm404@users.noreply.github.com>
2024-11-01 16:05:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ then
     fi
   done
 fi
-wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json -P ~/ccminer
+wget https://raw.githubusercontent.com/ShadowStorm404/Verus-Phone-Mining/main/config.json -P ~/ccminer
 
 if [ -f ~/ccminer/ccminer ]
 then
